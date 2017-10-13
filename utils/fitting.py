447de38163c3472e799c39c655361f7d4cc8e45a
mrayson <matt.rayson@gmail.com>
2017-10-13 12:02:31 +0800

    Added fitting and analysis routines

---
+++ utils/fitting.py
@@ -0,0 +1,191 @@
+"""
+Various wave profile fitting routines live here...
+"""
+import numpy as np
+from scipy.interpolate import PchipInterpolator
+import scipy.linalg as la
+from scipy.optimize import newton_krylov
+
+from iwaves import IWaveModes
+from iwaves.utils.density import FitDensity
+
+
+GRAV = 9.81
+RHO0 = 1024.
+
+def calc_buoyancy_h99(B, phi_1, c1, N2, dN2_dz, r10, T10, nonlinear=True):
+    """
+    Use the Holloway et al 99 version of the eqn's
+    """
+    #dN2_dz = np.gradient(N2, -np.abs(dz_s))
+    
+    # Linear term
+    b = B[:,np.newaxis] * phi_1 * N2
+    
+    # nonlinear terms
+    if nonlinear:
+        alpha = -2*c1*r10
+        b -= alpha/(2*c1)*B[:,np.newaxis]*phi_1*N2
+        b -= 0.5*dN2_dz*B[:,np.newaxis]**2. * phi_1**2.
+        b += c1*B[:,np.newaxis]**2. *N2 * T10
+        
+    return b
+
+
+def fit_bmodes_linear(rho, rhoz, z,  zmin, modes,\
+        Nz=100, density_func='single_tanh', full_output=True):
+    """
+    Compute the linear modal amplitude to the mode numbers in the list
+
+    Inputs:
+    ---
+        rho - matrix[nz, nt], density data
+        rhoz - vector[nz], background density profile from bottom to top (desceding)
+        z - vector[nz], depth from bottom to top, negative values (ascending)
+        modes - list[nmodes], mode numbers in python index i.e. 0=1
+
+    Returns
+    ---
+        A_t - matrix[nmodes, nt], modal amplitude
+        phi - matrix[nmodes, nz], modal structure functions
+        rhofit - matrix[nz, nt], best fit density profile
+
+    """
+
+    nz, nt = rho.shape
+    nmodes = len(modes)
+
+    # Compute buoyancy from density and backgroud density
+    rhopr = rho.T - rhoz[np.newaxis,...]
+    b = GRAV*rhopr/RHO0
+
+    # Compute the modal structures
+    L = np.zeros((nz,nmodes))
+    phi_n = []
+
+    # Calculate dz
+    Z = np.linspace(zmin, 0, Nz)
+    dz = np.mean(np.diff(Z))
+
+    for ii, mode in enumerate(modes):
+        # Use the mode class to create the profile
+        iw = IWaveModes(rhoz, z,\
+                density_class=FitDensity, density_func=density_func)
+        phi, c1, he, znew = iw(zmin, dz, mode)
+
+        if full_output:
+            if ii==0:
+                Nz = iw.Z.size
+                Lout = np.zeros((Nz, nmodes))
+            Lout[:,ii] = phi * iw.N2
+            phi_n.append(phi)
+
+        ## Interpolate the modal shape and N2 onto the measured depth locations
+        F = PchipInterpolator(iw.Z[::-1], iw.phi[::-1])
+        my_phi = F(z)
+
+        F = PchipInterpolator(iw.Z[::-1], iw.N2[::-1])
+        my_N2 = F(z)
+
+        L[:,ii] = my_phi*my_N2
+        #phi_n.append(my_phi)
+
+
+    ## Fit Ax=b
+    A_t,_,_,_ = la.lstsq(L , b.T)
+
+    # Reconstruct the density field
+    bfit_n = L[:,np.newaxis,:]*A_t.T[np.newaxis,...]
+    bfit = bfit_n.sum(axis=-1) # sum the modes
+
+    rhoprfit = bfit.T*RHO0/GRAV
+    rhofit = rhoprfit + rhoz[np.newaxis,:]
+
+    if full_output:
+        bfit_n = Lout[:,np.newaxis,:]*A_t.T[np.newaxis,...]
+        bfit = bfit_n.sum(axis=-1) # sum the modes
+        rhoprfit = bfit.T*RHO0/GRAV
+        rhofit_full = rhoprfit + iw.rhoZ[np.newaxis,:]
+        return A_t, np.array(phi_n).T, rhofit, rhofit_full, iw
+    else:
+        return A_t, np.array(phi_n).T, rhofit
+
+
+def fit_bmodes_nonlinear(rho, rhoz, z, mode, dz=2.5, density_func='single_tanh'):
+    """
+    Compute the nonlinear modal amplitude to the mode numbers in the list
+    
+
+    Inputs:
+    ---
+        rho - matrix[nz, nt], density data
+        rhoz - vector[nz], background density profile from bottom to top (desceding)
+        z - vector[nz], depth from bottom to top, negative values (ascending)
+        modes - list[nmodes], mode numbers in python index i.e. 0=1
+
+    Returns
+    ---
+        A_t - matrix[nmodes, nt], modal amplitude
+        rhofit - matrix[nz, nt], best fit density profile
+        iw - internal wave mode class
+
+    """
+    nz, nt = rho.shape
+    nmodes = len(modes)
+
+    # Compute buoyancy from density and backgroud density
+    rhopr = rho.T - rhoz[np.newaxis,...]
+    b = GRAV*rhopr/RHO0
+
+
+    # Use the mode class to create the profile
+    iw = IWaveModes(rhoz, z,\
+        density_class=FitDensity, density_func=density_func)
+    phi, c1, he, znew = iw(z.min(), dz, mode)
+
+    r10, _, _, _ = iw.calc_nonlin_params()
+    _, _, _, T10, _, _, _ = iw.calc_nonlin_structure()
+    
+    # Interpolate the structure etc
+    F = PchipInterpolator(iw.Z[::-1], iw.phi[::-1])
+    my_phi = F(z)
+
+    F = PchipInterpolator(iw.Z[::-1], iw.N2[::-1])
+    my_N2 = F(z)
+
+    # Interpolate the higher order structure function
+    F = PchipInterpolator(iw.Z[::-1], T10[::-1])
+    my_T10 = F(Z)
+    
+    # Calculate the gradient of N2 and then interpolate it...
+    dN2_dz = np.gradient(iw.N2, -iw.dz)
+    F = PchipInterpolator(iw.Z[::-1], dN2_dz[::-1])
+    my_dN2 = F(Z)
+    
+    ## Fit alpha as well
+    #alpha = -0.008*0
+    #my_T10 = my_T10s + alpha*my_phi
+    
+    def minfun(x0):
+        
+        btest = calc_buoyancy_h99(x0, my_phi, iw.c1, my_N2,\
+                my_dN2, r10, my_T10, nonlinear=False)
+    
+        err = np.sum( (b-btest)**2., axis=1)
+        #print err.max(), alpha
+        return err
+    
+    # Use the Newton-Krylov solver that works on large nonlinear problems
+    A_nl = newton_krylov(minfun, np.zeros((nt,)), f_tol=3.1e-5,\
+            method='gmres', rdiff=1e-6, iter=100)
+    
+    # Reconstruct the nonlinear density field
+    bfit_nl = calc_buoyancy_h99(A_nl, my_phi, iw.c1, my_N2,\
+                my_dN2, r10, my_T10, nonlinear=False)
+    
+    rhoprfit = bfit_nl*RHO0/GRAV
+    rhofitnl = rhoprfit + rhobar[np.newaxis,:]
+
+    return A_nl, rhofitnl, iw
+ 
+
---
+++ utils/minmax.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+def get_peak_window(y, t, windowsize, nmax, fun='max', vals=[], idxs=[], ctr=0):
+    """
+    Returns windowed peak values from a time series
+    
+    Inputs
+       y - input vector
+       t - time vector
+       windowsize - size of the window to remove each min/max (units samples)
+       nmax - number of maxima/minima to extract
+       fun - (optional) either max, min or maxabs
+       
+    Outputs
+        vals - list with size nmax with vectors of size windowsize
+        idxs - time step of each max value
+        ctr - counter (used for recursive call)
+       
+    """
+    
+    sz = y.size
+    
+    if fun == 'maxabs':
+        idx = np.argsort(np.abs(y))[::-1][0] # descending order
+    elif fun == 'max':
+        idx = np.argsort(y)[::-1][0] # descending order
+    elif fun == 'min':
+        idx = np.argsort(y)[0] # ascending order
+    else:
+        raise Exception, 'unknown function', fun
+    
+    # Find the start and end indices to extract from the initial time series
+    i1 = max(0, idx-windowsize//2)
+    i2 = min(sz, idx+windowsize//2)
+
+    # Init a zero-padded output window
+    yout = np.zeros((windowsize,))
+    
+    # We want to put the peak at the halfway point
+    if i2 == sz:
+        istart = np.mod(i1-sz,windowsize)
+        iend=windowsize
+        #istart = sz - (i2-i1)
+        #iend = windowsize 
+    elif i1 == 0:
+        istart = windowsize - i2
+        iend = windowsize
+    else:
+        istart=0
+        iend=windowsize
+    
+    #print idx, i1, i2, sz, istart, iend, y[i1:i2].shape,
+    
+    # Insert the peak into the yout array
+    yout[istart:iend] = y[i1:i2]
+    vals.append(yout)
+    idxs.append(t[idx])
+    
+    # Mask the original array to remove thes maximum values
+    mask = np.ones_like(y).astype(np.bool)
+    mask[i1:i2] = False
+    
+    # Recursively call the function
+    ctr+=1
+    if ctr < nmax:
+        vals, idxs, ctr = get_peak_window(y[mask], t[mask], windowsize, nmax,\
+                        vals=vals, idxs=idxs, ctr=ctr, fun=fun)
+    
+    return vals, idxs, ctr
