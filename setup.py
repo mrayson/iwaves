b0a3e3dd5bb20226615c388f395ba289aa5c21de
matt.rayson <matt.rayson@gmail.com>
2018-11-02 15:53:18 +0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup #, find_packages
 
 setup(name='iwaves',
       version='latest',
       description='Internal wave KdV solver',
       author='Matt Rayson',
       author_email='matt.rayson@uwa.edu.au',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      #packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+	  packages=['iwaves.utils','iwaves.kdv']
       license='LICENSE',
     )
