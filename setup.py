25636e032a1f78792a8e401543296609d22f15f2
matt.rayson <matt.rayson@gmail.com>
2018-11-02 15:35:54 +0800

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='iwaves',
+      version='latest',
+      description='Internal wave KdV solver',
+      author='Matt Rayson',
+      author_email='matt.rayson@uwa.edu.au',
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      license='LICENSE',
+    )
